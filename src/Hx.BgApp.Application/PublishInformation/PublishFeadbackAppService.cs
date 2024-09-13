01fb98f23e88d4710540d6ea2d261d5315123776
hys <243835440@qq.com>
2024-09-13 22:22:03 +0800

    feat(convet url): image url convet

--- src/Hx.BgApp.Application/PublishInformation/PublishFeadbackAppService.cs
+++ src/Hx.BgApp.Application/PublishInformation/PublishFeadbackAppService.cs
@@ -1,4 +1,5 @@
-﻿using NUglify.Helpers;
+﻿using Microsoft.Extensions.Configuration;
+using NUglify.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,13 @@ namespace Hx.BgApp.PublishInformation
     public class PublishFeadbackAppService : BgAppAppService
     {
         protected IEfCorePublishFeadbackRepository PublishFeadbackRepository { get; }
-        public PublishFeadbackAppService(IEfCorePublishFeadbackRepository publishFeadbackRepository)
+        protected IConfiguration Configuration { get; }
+        public PublishFeadbackAppService(
+            IEfCorePublishFeadbackRepository publishFeadbackRepository,
+            IConfiguration configuration)
         {
             PublishFeadbackRepository = publishFeadbackRepository;
+            Configuration = configuration;
         }
         public async Task CreateAsync(PublishInfoCreateDto input)
         {
@@ -61,19 +66,63 @@ namespace Hx.BgApp.PublishInformation
         {
             var list = await PublishFeadbackRepository.GetListAsync(input.MaxResultCount, input.SkipCount, input.Filter);
             var count = await PublishFeadbackRepository.GetCountAsync(input.Filter);
-            return new PagedResultDto<PublishFeadbackInfoDto>(
-                count,
-                ObjectMapper.Map<List<PublishFeadbackInfo>, List<PublishFeadbackInfoDto>>(list));
+            var result = ObjectMapper.Map<List<PublishFeadbackInfo>, List<PublishFeadbackInfoDto>>(list);
+            foreach (var item in result)
+            {
+                foreach (var feadbackInfo in item.FeadbackInfos)
+                {
+                    feadbackInfo.LivingEnvironmentPics = ConvertSrc(feadbackInfo.LivingEnvironmentPics);
+                    feadbackInfo.ObserveLawPics = ConvertSrc(feadbackInfo.ObserveLawPics);
+                    feadbackInfo.NeighborsPics = ConvertSrc(feadbackInfo.NeighborsPics);
+                    feadbackInfo.FamilyTraditionPics = ConvertSrc(feadbackInfo.FamilyTraditionPics);
+                    feadbackInfo.GettingRichPics = ConvertSrc(feadbackInfo.GettingRichPics);
+                    feadbackInfo.PolicyImplementationPics = ConvertSrc(feadbackInfo.PolicyImplementationPics);
+                    feadbackInfo.PublicSpiritedPics = ConvertSrc(feadbackInfo.PublicSpiritedPics);
+                    feadbackInfo.CivilizedPics = ConvertSrc(feadbackInfo.CivilizedPics);
+                }
+            }
+            return new PagedResultDto<PublishFeadbackInfoDto>(count, result);
         }
         public async Task<List<PublishFeadbackInfoDto>> GetListAsync()
         {
             var list = await PublishFeadbackRepository.GetListAsync();
-            return ObjectMapper.Map<List<PublishFeadbackInfo>, List<PublishFeadbackInfoDto>>(list);
+            var result = ObjectMapper.Map<List<PublishFeadbackInfo>, List<PublishFeadbackInfoDto>>(list);
+            foreach (var item in result)
+            {
+                foreach (var feadbackInfo in item.FeadbackInfos)
+                {
+                    feadbackInfo.LivingEnvironmentPics = ConvertSrc(feadbackInfo.LivingEnvironmentPics);
+                    feadbackInfo.ObserveLawPics = ConvertSrc(feadbackInfo.ObserveLawPics);
+                    feadbackInfo.NeighborsPics = ConvertSrc(feadbackInfo.NeighborsPics);
+                    feadbackInfo.FamilyTraditionPics = ConvertSrc(feadbackInfo.FamilyTraditionPics);
+                    feadbackInfo.GettingRichPics = ConvertSrc(feadbackInfo.GettingRichPics);
+                    feadbackInfo.PolicyImplementationPics = ConvertSrc(feadbackInfo.PolicyImplementationPics);
+                    feadbackInfo.PublicSpiritedPics = ConvertSrc(feadbackInfo.PublicSpiritedPics);
+                    feadbackInfo.CivilizedPics = ConvertSrc(feadbackInfo.CivilizedPics);
+                }
+            }
+            return result;
         }
         public async Task<PublishFeadbackInfoDto?> GetAsync(Guid id)
         {
             var publish = await PublishFeadbackRepository.FindAsync(id);
-            return ObjectMapper.Map<PublishFeadbackInfo?, PublishFeadbackInfoDto?>(publish);
+            var result = ObjectMapper.Map<PublishFeadbackInfo?, PublishFeadbackInfoDto?>(publish);
+            return result;
+        }
+        private string ConvertSrc(string pics)
+        {
+            var src = "";
+            if (!string.IsNullOrEmpty(pics))
+            {
+                foreach (var temp in pics.Split(','))
+                {
+                    var srcTemp = temp.Contains(AppGlobalProperties.StaticFilesBasePathSign) ?
+                        temp.Replace(AppGlobalProperties.StaticFilesBasePathSign, Configuration[AppGlobalProperties.FileServerBasePath]) : "";
+                    if (string.IsNullOrEmpty(src))
+                        src += $"{srcTemp}";
+                }
+            }
+            return src;
         }
     }
 }
